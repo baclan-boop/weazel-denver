dcafb9aef2c670f78431d353eaffbbed22a609ba
Vladislav <computer52552@gmail.com>
2026-07-26 02:36:35 +0300

    покраска отдельных фрагментов текста в новостях

--- src/src/middleware/rateLimiters.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-const rateLimit = require('express-rate-limit');
-const { hashIP } = require('../utils/helpers');
-
-const loginLimiter = rateLimit({ windowMs: 15 * 60 * 1000, max: 10, message: { error: 'Слишком много попыток. Подождите 15 минут.' }, keyGenerator: r => hashIP(r.ip) });
-const apiLimiter = rateLimit({ windowMs: 60 * 1000, max: 200, message: { error: 'Слишком много запросов.' }, keyGenerator: r => hashIP(r.ip) });
-
-module.exports = { loginLimiter, apiLimiter };
